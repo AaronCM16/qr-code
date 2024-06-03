0e90bde724a5d9c5589bbbaae8f96c5b2093e377
Aaron <aaronoso0704@gmail.com>
2024-06-03 10:05:08 -0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 z-10 w-full py-1 text-center shadow-lg">
       <p className="mx-auto flex w-max items-center justify-center rounded-md bg-slate-800 p-1 text-sm font-medium text-white">
-        &copy; {new Date().getFullYear()} QR Proyect. All rights reserved.
+        &copy; {new Date().getFullYear()} QR Proyect. Todos los derechos
+        reservados.
       </p>
     </footer>
   );
